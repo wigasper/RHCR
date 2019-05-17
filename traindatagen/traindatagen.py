d20c3a21491e6d3968a8c103402018edddb5b8a6
Kirk Gasper <wigasper@gmail.com>
2019-05-16 19:26:40 -0500

    improved basic functionality/modularity, lowered default line width

--- traindatagen/traindatagen.py
+++ traindatagen/traindatagen.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import re
 import sys
+import time
 import logging
 import argparse
 
@@ -12,7 +14,7 @@ import numpy as np
 # are reasonable if input doc does not have newlines
 # Adds words to lines until the next word would put it over the max width
 # Then yields the line
-def format_line(line, max_width=120):
+def format_line(line, max_width=90):
     line = line.split()
     while len(line) > 0:
         out = line[0]
@@ -26,48 +28,63 @@ class traindatagen:
     
     def main():
         doc = []
+        
+        # Get command line args
         parser = argparse.ArgumentParser()
         parser.add_argument("-i", "--input", help="input file path")
+        parser.add_argument("-o", "--output", help="output file path")
         args = parser.parse_args()
         
         # Set up logger
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
-        handler = logging.FileHandler("./traindatagen/traindatagen.log")
+        handler = logging.FileHandler("traindatagen.log")
         formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
         logger.addHandler(handler)
 
+        # Output formats supported by matplotlib
+        supported_formats = ["eps", "jpeg", "jpg", "pdf", "pgf", "png", "ps", 
+                             "raw", "rgba", "svg", "svgz", "tif", "tiff"]
             
         if args.input:
+            in_path = args.input
             # Read in document to transform to cursive
-            with open(args.input, "r") as handle:
+            with open(in_path, "r") as handle:
                 for line in handle:
                     for frmtd_line in format_line(line):
                         doc.append(frmtd_line)
         else:
+            in_path = "sys.stdin"
             # haven't tested sys.sdtin usage
             for line in sys.stdin.read():
                 for frmtd_line in format_line(line):
                     doc.append(frmtd_line)
-
+        
+        if args.output:
+            out_path = args.output
+            if out_path.split(".")[-1] not in supported_formats:
+                out_path = "".join([out_path, ".png"])
+        else:
+            out_path = "{}.png".format(time.asctime())
+            
         #########################################
         # for testing in IDE, read in doc:
-        with open("./traindatagen/cyrillic.3.test", "r") as handle:
-                for line in handle:
-                    for frmtd_line in format_line(line):
-                        doc.append(frmtd_line)
+#        with open("./traindatagen/cyrillic.3.test", "r") as handle:
+#                for line in handle:
+#                    for frmtd_line in format_line(line):
+#                        doc.append(frmtd_line)
         #########################################
         
         # Build dict
         letters_dict = {}
-        with open("./traindatagen/letters.dict", "r") as handle:
+        with open("letters.dict", "r") as handle:
             for line in handle:
                 line = line.strip("\n").split(",")
                 letters_dict[line[0]] = line[1]
         
         for letter in letters_dict:
-            fp ="./traindatagen/letters/{}.png".format(letters_dict[letter])
+            fp ="letters/{}.png".format(letters_dict[letter])
             letters_dict[letter] = np.array(load_img(fp, color_mode="grayscale"))
         
         # Whitespace, should make this variable for inconsistent spacing
@@ -98,7 +115,8 @@ class traindatagen:
         # Combine vertically
         out = np.vstack(tuple(out))
         
-        plt.imsave("./traindatagen/test3.jpg", out, cmap='gray')
+        plt.imsave("{}".format(out_path), out, cmap='gray')
+        logger.info("Translated {} to {}".format(in_path, out_path))
         
     if __name__ == '__main__':
         main()
