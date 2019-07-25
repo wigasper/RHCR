a03df58ea1aca6936b332f6763a0493bed811859
rgasper <raymond.j.gasper@gmail.com>
2019-07-24 21:40:48 -0400

    Cleaning up old junk, update .gitignore

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -1,3 +1,5 @@
+#### WARNING DEPRECATED USING TENSORFLOW 2 NOW ###
+print("WARNING DEPRECATED USING TENSORFLOW 2 NOW") 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
