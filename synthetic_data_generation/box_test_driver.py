5d9fa85c1e0adf350cdea339125c394d2f4c7cec
rgasper <raymond.j.gasper@gmail.com>
2019-08-05 08:30:40 -0400

    rationalize box_test_driver

--- synthetic_data_generation/box_test_driver.py
+++ synthetic_data_generation/box_test_driver.py
@@ -1,23 +1,32 @@
 import argparse
-
 from PIL import Image, ImageDraw
 
-def main():
+def box_drawer(imageFile, targetsFile, outFile):
+    '''takes the saved letter selection boxes (defined in the targetsFile) 
+    and draws them over the imageFile for human visual inspection'''
     rect_coords = []
 
-    with open("responses.tab", "r") as handle:
+    with open(targetsFile, "r") as handle:
         for line in handle:
             line = line.split("\t")
             rect_coords.append([int(line[1]), int(line[2]), int(line[3]), int(line[4])])
     
-    img = Image.open("0000051.png")
+    img = Image.open(imageFile)
     draw = ImageDraw.Draw(img)
 
     for coord in rect_coords:
         draw.rectangle([coord[0], coord[1], coord[2], coord[3]], fill=None,
                         outline=0, width=3)
-    
-    img.save("box_test.png")
+
+    img.save(outFile)
+
+def getArgs():
+    ''' used to get command-line arguments when run standalone '''
+    parser = argparse.ArgumentParser(description='draws bounding boxes from target file over image for human inspection')
+    parser.add_argument('imageFile', type=str, help='image to draw boxes over')
+    parser.add_argument('targetsFile', type=str, help='file with box coords, in standard format defined in repo README')
+    parser.add_argument('outFile', type=str, help='output image file, suggested format is .png')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    main()
+    args = getArgs()
